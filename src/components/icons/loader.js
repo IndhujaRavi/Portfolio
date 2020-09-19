bd1bc1c48c877e9284b03c92509450bec1cfafe7
Indhuja <indhujaravi10@gmail.com>
2020-09-19 16:46:09 -0700

    Changes to loader

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,23 +1,29 @@
 import React from 'react';
 
 const IconLoader = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-    <title>Loader Logo</title>
-    <g>
-      <g id="B" transform="translate(11.000000, 5.000000)">
-      <text transform="translate(350 310)" fill="#63ffda" font-size="150" font-family="Helvetica" font-weight="600"><tspan x="0" y="0">I</tspan></text><path d="M435.651,850.174l126.064-68.158V637.388L437.036,563.135,310.695,632.124V776.474Z" transform="translate(-67 -450)" fill="none" stroke="#63ffda" stroke-width="10"/>
-      </g>
+  <svg
+    id="logo"
+    xmlns="http://www.w3.org/2000/svg"
+    width="261.021"
+    height="298.536"
+    viewBox="0 0 261.021 298.536">
+    <g transform="translate(-238.695 -107.379)">
+      <text
+        transform="translate(350 310)"
+        fill="#63ffda"
+        fontSize="150"
+        fontFamily="Helvetica"
+        fontWeight="600">
+        <tspan x="0" y="0">
+          I
+        </tspan>
+      </text>
       <path
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
+        d="M435.651,850.174l126.064-68.158V637.388L437.036,563.135,310.695,632.124V776.474Z"
+        transform="translate(-67 -450)"
+        fill="none"
+        stroke="#63ffda"
+        strokeWidth="10"
       />
     </g>
   </svg>
